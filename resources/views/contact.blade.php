b6a464ed17a258dd1cb2c6cf1bb59cd00057900e
Md Abu Sufian <63811452+mdabusufian@users.noreply.github.com>
2022-07-21 20:13:16 +0600

    Delete resources directory

--- resources/views/contact.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layout.app')
-@section('content')
-
-<h1 style="text-align: center;">Welcome {{$name}}</h1>
- <p style="text-align: center;">{{$text}}</p>
-
-@endsection
--- resources/views/inc/topnav.blade.php
+++
@@ -1,5 +0,0 @@
-<p style="text-align: center;"><a class="btn btn-primary" href="{{route('Home')}}">Home</a>
-<a class="btn btn-primary" href="{{route('service')}}">Service</a>
-<a class="btn btn-primary" href="{{route('our')}}">Our Teams</a>
-<a class="btn btn-primary" href="{{route('about')}}">About US</a>
-<a class="btn btn-primary" href="{{route('contact')}}">Contact US</a></p>
